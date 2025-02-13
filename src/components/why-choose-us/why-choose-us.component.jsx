83937be009c76f3371031598197e991ba112c2b7
Ali Rida Darsa <milotheapache@gmail.com>
2025-02-13 19:01:43 +0200

    fix: Update image paths in AboutUs, PartnerCarousel, and WhyChooseUs components for consistency

--- src/components/why-choose-us/why-choose-us.component.jsx
+++ src/components/why-choose-us/why-choose-us.component.jsx
@@ -35,7 +35,7 @@ const WhyChooseUs = () => {
             <h2 className="heading">Why Choose Us?</h2>
    
             <ImageTextSection
-                imageUrl="/src/assets/images/components/why-choose-us/application.webp"
+                imageUrl="src\assets\images\components\why-choose-us\application.webp"
                 title="Expert Team"
                 description="Our team of experts is dedicated to delivering top-notch solutions tailored to your needs."
                 layout="image-text"
@@ -43,7 +43,7 @@ const WhyChooseUs = () => {
             />
    
             <ImageTextSection
-                imageUrl="/src/assets/images/components/why-choose-us/application2.webp"
+                imageUrl="src\assets\images\components\why-choose-us\application2.webp"
                 title="Innovative Solutions"
                 description="We leverage the latest technologies to provide innovative and scalable solutions for your business."
                 layout="text-image"
@@ -51,7 +51,7 @@ const WhyChooseUs = () => {
             />
    
             <ImageTextSection
-                imageUrl="/src/assets/images/components/why-choose-us/application3.webp"
+                imageUrl="src\assets\images\components\why-choose-us\application3.webp"
                 title="Customer-Centric Approach"
                 description="We prioritize your needs and ensure seamless communication throughout the project lifecycle."
                 layout="image-text"
